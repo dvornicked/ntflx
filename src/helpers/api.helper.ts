8de5475edcd6feb18ebd6bb1f3b6db931565369c
Dvornick <mail@dvornick.ru>
2022-08-27 15:19:54 +0300

    Add basic endpoints to api helper

---
+++ src/helpers/api.helper.ts
@@ -0,0 +1,13 @@
+const API_URL = `${process.env.APP_URL}/api`
+
+export const API = {
+	users: `${API_URL}/users`,
+	profile: `${API_URL}/profile`,
+	password: `${API_URL}/password`,
+	register: `${API_URL}/register`,
+	login: `${API_URL}/login`,
+	films: `${API_URL}/films`,
+	actors: `${API_URL}/actors`,
+	genres: `${API_URL}/genres`,
+	upload: `${API_URL}/upload`,
+}
