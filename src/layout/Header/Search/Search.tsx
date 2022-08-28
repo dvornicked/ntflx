34176c76a893e4ea53a5846bfb5d88e25e1cff22
Dvornick <mail@dvornick.ru>
2022-08-28 07:16:27 +0300

    Improve search

--- src/layout/Header/Search/Search.tsx
+++ src/layout/Header/Search/Search.tsx
@@ -14,15 +14,16 @@ const Search = (props: ISearchProps) => {
 	return (
 		<div
 			sx={{
-				transition: 'width 0.3s ease-in-out',
-				width: searchValue === 'Search' ? 'auto' : '100%',
+				width: '100%',
+				transition: 'width .3s ease-in-out',
 				height: '100%',
 				display: 'flex',
+				justifyContent: 'end',
 				alignItems: 'center',
 				lineHeight: 0,
 				cursor: 'pointer',
-				px: 2,
-				border: '2px solid',
+				pr: 2,
+				borderRight: '2px solid',
 				borderImage:
 					'linear-gradient(to bottom, transparent 5%, rgba(80,80,80,.5) 50%, transparent 95%) 1',
 				position: 'relative',
@@ -31,7 +32,12 @@ const Search = (props: ISearchProps) => {
 		>
 			<IoSearch
 				sx={{
+					width: 34,
 					height: '100%',
+					borderLeft: '2px solid',
+					borderImage:
+						'linear-gradient(to bottom, transparent 5%, rgba(80,80,80,.5) 50%, transparent 95%) 1',
+					pl: 2,
 				}}
 				onClick={() =>
 					searchValue
@@ -43,33 +49,43 @@ const Search = (props: ISearchProps) => {
 				value={searchValue}
 				sx={{
 					width: searchValue === 'Search' ? '70px' : '100%',
+					transition: 'width .3s ease-in-out',
 					height: '100%',
 					fontSize: 2,
 					bg: 'transparent',
 					border: 'none',
 					color: 'text',
 					px: 2,
+					position: 'relative',
 				}}
 				onFocus={() => setSearchValue('')}
-				onBlur={() => setSearchValue('Search')}
+				onBlur={e => {
+					if (e.target.value === '') setSearchValue('Search')
+				}}
 				onChange={e => setSearchValue(e.target.value)}
 			/>
 			{searchValue !== 'Search' &&
 				(isLoading ? (
 					<Spinner size={24} color="white" />
 				) : (
-					<IoClose
-						sx={{
-							height: '100%',
-							justifySelf: 'end',
-							position: 'relative',
-						}}
-						onClick={() => setSearchValue('Search')}
-					/>
+					<div onClick={() => setSearchValue('Search')}>
+						<IoClose
+							sx={{
+								height: '100%',
+								justifySelf: 'end',
+								position: 'relative',
+							}}
+						/>
+					</div>
 				))}
-			{searchValue !== ('Search' || '') && searchValue.length > 3 && (
-				<SearchList value={debouncedSearchValue} setIsLoading={setIsLoading} />
-			)}
+			{searchValue !== ('Search' || '') &&
+				searchValue.length > 3 &&
+				searchValue === debouncedSearchValue && (
+					<SearchList
+						value={debouncedSearchValue}
+						setIsLoading={setIsLoading}
+					/>
+				)}
 		</div>
 	)
 }
